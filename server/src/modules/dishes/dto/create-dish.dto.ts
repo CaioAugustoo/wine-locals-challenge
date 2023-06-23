ddf64b338917b2e57d38695618f9a6a94db3b32e
caioaugustoo <caioamfr@gmail.com>
2023-06-22 22:28:42 -0300

    chore: mark schemas as strict

--- server/src/modules/dishes/dto/create-dish.dto.ts
+++ server/src/modules/dishes/dto/create-dish.dto.ts
@@ -1,26 +1,28 @@
 import { Dish } from '@prisma/client';
 import { z } from 'zod';
 
-export const createDishDtoSchema = z.object({
-  name: z
-    .string({
-      required_error: "The 'name' field is required.",
-    })
-    .min(3)
-    .max(50),
-  price: z.number({
-    required_error: "The 'price' field is required.",
-  }),
-  description: z
-    .string({
-      required_error: "The 'description' field is required.",
-    })
-    .min(3)
-    .max(255),
-  restaurantId: z.string({
-    required_error: "The 'restaurantId' field is required.",
-  }),
-});
+export const createDishDtoSchema = z
+  .object({
+    name: z
+      .string({
+        required_error: "The 'name' field is required.",
+      })
+      .min(3)
+      .max(50),
+    price: z.number({
+      required_error: "The 'price' field is required.",
+    }),
+    description: z
+      .string({
+        required_error: "The 'description' field is required.",
+      })
+      .min(3)
+      .max(255),
+    restaurantId: z.string({
+      required_error: "The 'restaurantId' field is required.",
+    }),
+  })
+  .strict();
 
 export type CreateDishDto = z.infer<typeof createDishDtoSchema>;
 export type CreateDishDtoOutput = {
