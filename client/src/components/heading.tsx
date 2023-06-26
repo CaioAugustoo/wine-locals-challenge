fc4147095d888fb903cce6ad88dad67893e7d10f
caioaugustoo <caioamfr@gmail.com>
2023-06-25 22:33:06 -0300

    styles: break words

--- client/src/components/heading.tsx
+++ client/src/components/heading.tsx
@@ -6,7 +6,9 @@ interface HeadingProps {
 export const Heading = ({ title, description }: HeadingProps) => {
   return (
     <>
-      <h1 className="text-left text-4xl font-bold text-yellow-500">{title}</h1>
+      <h1 className="break-words text-left text-4xl font-bold text-yellow-500">
+        {title}
+      </h1>
       {!!description && <p className="font-light">{description}</p>}
     </>
   );
