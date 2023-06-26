f95e9e108464888f69edaaa0cb048ac241873885
caioaugustoo <caioamfr@gmail.com>
2023-06-25 22:00:51 -0300

    fix: typescript

--- client/src/components/restaurant/title.tsx
+++ client/src/components/restaurant/title.tsx
@@ -1,10 +1,10 @@
 import { Heading } from "@/src/components";
 
 interface TitleProps {
-  name: string;
-  totalDishes: number;
+  name: string | undefined;
+  totalDishes: number | undefined;
 }
 
-export const Title = ({ name, totalDishes = 0 }: TitleProps) => {
+export const Title = ({ name = "", totalDishes = 0 }: TitleProps) => {
   return <Heading title={name} description={`${totalDishes} pratos`} />;
 };
